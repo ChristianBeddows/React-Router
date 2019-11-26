d4c826e01ee794b8b78cff9817791d2ef40f0fbf
Christian Beddows <christian.beddows@york.ac.uk>
2019-11-26 17:08:51 +0000

    Make form stateful

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,27 @@ import "./Form.css";
 
 class Form extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            forename: null,
+            surname: null,
+            email: null,
+            affiliation: null,
+            dateOfBirth: null,
+            bio: null,
+        }
+    }
+
+    handleChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
     render() {
         return (
             <form className="form">
@@ -10,49 +31,106 @@ class Form extends React.Component {
                 <h2 className="form-title">Enter your Details</h2>
 
                 <div className="form-row">
-                    <div className="form-field half">
-                        <label for="forename">Forename</label>
-                        <input id="forename" type="text"/>
-                    </div>
-
-                    <div className="form-field half">
-                        <label for="surname">Surname</label>
-                        <input id="surname" type="text"/>
-                    </div>
+                    <Forename
+                        forename={this.state.forename}
+                        size="half"
+                        onChange={(event) => this.handleChange(event)}
+                    />
+                    <Surname
+                        surname={this.state.surname}
+                        size="half"
+                        onChange={(event) => this.handleChange(event)}
+                    />
                 </div>
 
                 <div className="form-row">
-                    <div className="form-field full">
-                        <label for="email">Email Address</label>
-                        <input id="email" type="text"/>
-                    </div>
+                    <Email
+                        email={this.state.email}
+                        size="full"
+                        onChange={(event) => this.handleChange(event)}
+                    />
                 </div>
 
                 <div className="form-row">
-                    <div className="form-field half">
-                        <label>Affiliation</label><br/>
-                        <input className="form-radio-input" type="radio" name="affiliation" value="staff"/>Staff<br/>
-                        <input className="form-radio-input" type="radio" name="affiliation" value="student"/>Student<br/>
-                        <input className="form-radio-input" type="radio" name="affiliation" value="associate"/>Associate<br/>
-                    </div>
-
-                    <div className="form-field half">
-                        <label for="dob">Date of Birth</label>
-                        <input id="dob" type="date"/>
-                    </div>
+                    <Affiliations
+                        size="half"
+                        onChange={(event) => this.handleChange(event)}
+                    />
+                    <DOB
+                        dateOfBirth={this.state.dateOfBirth}
+                        size="half"
+                        onChange={(event) => this.handleChange(event)}
+                    />
                 </div>
 
                 <div className="form-row">
-                    <div className="form-field full">
-                        <label for="bio">Bio</label>
-                        <input id="bio" type="textarea" className="form-textarea-input"/>
-                    </div>
+                    <Bio
+                        bio={this.state.bio}
+                        size="full"
+                        onChange={(event) => this.handleChange(event)}
+                    />
                 </div>
 
                 <button className="form-submit">Submit</button>
             </form>
         );
     }
+}
+
+function Forename(props) {
+        return (
+            <div className={"form-field " + props.size}>
+                <label htmlFor="forename">Forename</label>
+                <input id="forename" name="forename" type="text" value={props.forename} onChange={props.onChange}/>
+            </div>
+        );
+}
+
+function Surname(props) {
+    return (
+        <div className={"form-field " + props.size}>
+            <label htmlFor="surname">Surname</label>
+            <input id="surname" name="surname" type="text" value={props.surname} onChange={props.onChange}/>
+        </div>
+    );
+}
+
+function Email(props) {
+    return (
+        <div className={"form-field " + props.size}>
+            <label htmlFor="email">Email Address</label>
+            <input id="email" name="email" type="text" value={props.email} onChange={props.onChange}/>
+        </div>
+    );
+}
+
+function DOB(props) {
+    return (
+        <div className={"form-field " + props.size}>
+            <label htmlFor="dob">Date of Birth</label>
+            <input id="dob" name="dateOfBirth" type="date" value={props.dateOfBirth} onChange={props.onChange}/>
+        </div>
+    );
+}
+
+function Affiliations(props) {
+    return (
+        <div className={"form-field " + props.size}>
+            <label>Affiliation</label><br/>
+            <input className="form-radio-input" type="radio" name="affiliation" value="staff" onChange={props.onChange}/>Staff<br/>
+            <input className="form-radio-input" type="radio" name="affiliation" value="student" onChange={props.onChange}/>Student<br/>
+            <input className="form-radio-input" type="radio" name="affiliation" value="associate" onChange={props.onChange}/>Associate<br/>
+        </div>
+    );
+}
+
+function Bio(props) {
+    return (
+        <div className={"form-field " + props.size}>
+            <label htmlFor="bio">Bio</label>
+            <input id="bio" name="bio" type="textarea" className="form-textarea-input" value={props.bio} onChange={props.onChange}/>
+        </div>
+    );
 }
 
 export default Form;
