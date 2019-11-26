36023a2a7263b0b6b2606f71c38dece614df5ff6
Christian Beddows <christian.beddows@york.ac.uk>
2019-11-26 14:54:55 +0000

    Basic app design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <div className="page-body">
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/about">
+                        <About/>
+                    </Route>
+                    <Route path="/users">
+                        <Users/>
+                    </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
-export default App;
+function NavBar() {
+    return (
+        <nav className="navigation-bar">
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/users">Users</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+function Home() {
+    return (
+        <div>
+            <h1 className="page-title">Home</h1>
+            <NavBar/>
+        </div>
+    );
+}
+
+function About() {
+    return (
+        <div>
+            <h1 className="page-title">About</h1>
+            <NavBar/>
+        </div>
+    );
+}
+
+function Users() {
+    return (
+        <div>
+            <h1 className="page-title">Users</h1>
+            <NavBar/>
+        </div>
+    );
+}
